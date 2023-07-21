9cac3f672af9f9fb19619ce77e2edddad28e2348
yuzq <1065265077@qq.com>
2023-07-21 11:26:49 +0800

    add

--- Test/Test2.py
+++ Test/Test2.py
@@ -17,16 +17,20 @@ div_list_post = soup.find_all('div', {'class': 'list-post'})
 for post in div_list_post:
     article_tags = post.find_all('article')
     # 遍历所有的<a>标签，获取href和title属性
+
     for article in article_tags:
-        a_tag2 = article.find('h2')
-        a_tag = a_tag2.find('a')
-        href = a_tag['href']
-        title = a_tag['title']
-        if href is not None:
-            print(article['id'])
-            print('Link:', href)
-            print('Title:', title)
-            PostTem.postTem(article['id'], href)
+        try:
+            a_tag2 = article.find('h2')
+            a_tag = a_tag2.find('a')
+            href = a_tag['href']
+            title = a_tag['title']
+            if href is not None:
+                print(article['id'])
+                print('Link:', href)
+                print('Title:', title)
+                PostTem.postTem(article['id'], href)
+        except AttributeError as e:
+            print('出现异常了')
 
 
 
