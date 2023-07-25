729ee10b5781237a5bd6d6cf77d2a92b7c1fa2b2
yuzq <1065265077@qq.com>
2023-07-25 19:05:01 +0800

    add

--- Test/TestStr.py
+++ Test/TestStr.py
@@ -1,6 +1,8 @@
 
-first_level_p_tags = ['i', 'am', 'tom']
-content = '  \n'.join([" " + p.strip() for p in first_level_p_tags])
-content2 = '  \n'.join([" " + p for p in first_level_p_tags])
-print(content)
-print(content2)
+for i in range(26, 40):
+    print(i)
+# first_level_p_tags = ['i', 'am', 'tom']
+# content = '  \n'.join([" " + p.strip() for p in first_level_p_tags])
+# content2 = '  \n'.join([" " + p for p in first_level_p_tags])
+# print(content)
+# print(content2)
